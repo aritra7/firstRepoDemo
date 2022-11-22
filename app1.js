8ad2a186dcc6f927e2886ef4b489cc45981f5425
aritra7 <kunduaritra7@gmail.com>
2022-11-22 20:19:12 +0530

    task 4 added

--- app1.js
+++ app1.js
@@ -24,3 +24,17 @@ header.style.borderBottom = 'black solid 2px'
 var addItems = document.getElementById('titleid')
 addItems.style.color = "green"
 addItems.style.fontSize = 'bold'
+
+// get elements by class name
+let items = document.getElementsByClassName('list-group-item')
+console.log(items)
+console.log(items[1])
+items[1].textContent = 'Hello 2'
+items[1].style.fontWeight = 'bold'
+items[1].style.backgroundColor = 'yellow'
+
+items[2].style.backgroundColor = 'green'
+
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold'
+}

e0d33eb390991056bca707f604debd63b6381b5a
aritra7 <kunduaritra7@gmail.com>
2022-11-24 01:27:58 +0530

    Task 11 Added with customed object declr

--- event.js
+++ event.js
@@ -2,10 +2,21 @@ let myForm = document.querySelector('.my-form')
 let inputName = document.getElementById('name')
 let inputEmail = document.getElementById('lname')
 
+
+var myObj ={
+    name: 'William',
+    email: 'Smith'
+}
+
+let myObj_serialized = JSON.stringify(myObj);
+
+
 myForm.addEventListener('submit',onSubmit);
 
 function onSubmit(e){
     e.preventDefault();
-    localStorage.setItem('userName',inputName.value)
-    localStorage.setItem('userEmail',inputEmail.value)
+    // localStorage.setItem('userName',inputName.value)
+    // localStorage.setItem('userEmail',inputEmail.value)
+    localStorage.setItem('myObj',myObj_serialized);
+    
 }
